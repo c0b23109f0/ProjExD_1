6e87bbaeddd14f7eaca617ae3e78ab2712d543cf
Toki <c0b23109f0@edu.teu.ac.jp>
2024-09-24 16:26:19 +0900

    キーボード操作可能

--- pygame_image.py
+++ pygame_image.py
@@ -13,19 +13,33 @@ def main():
     kk_img = pg.image.load("fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
     haikei_img = pg.transform.flip(bg_img, True, False) #練習7-1
+    kk_rct = kk_img.get_rect() #練習8-1
+    kk_rct.center = 300, 200 #練習8-1
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, +1))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1, 0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+
         x = -(tmr % 3200) #練習6
         screen.blit(bg_img, [x, 0])
         screen.blit(haikei_img, [x+1600, 0]) #練習7-1
         screen.blit(bg_img, [x+3200, 0]) #練習7-2
         screen.blit(haikei_img, [x+4800, 0]) #練習7-2
-        screen.blit(kk_img, [300, 200]) #練習4
+        screen.blit(kk_img, kk_rct) #練習4 ->　練習8-5
         pg.display.update()
         tmr += 1        
-        clock.tick(20000) #練習5
+        clock.tick(200) #練習5
 
 
 if __name__ == "__main__":
